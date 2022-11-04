4504efb473c002f6a4c4a86c0c44d1769cf7a454
Dmitriy <kravchenkodmitriy38@gmail.com>
2022-11-04 17:53:15 +0300

    Initial commit

---
+++ src/WordsChecker.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class WordsChecker {
+    public String text;
+    Set<String> set = new HashSet<>();
+
+    public WordsChecker(String text){
+        this.text = text;
+
+        for (String searchWord: text.split("\\P{IsAlphabetic}")){
+            set.add(searchWord);
+        }
+    }
+
+    public boolean hasWord(String searchWord){
+        return set.contains(searchWord);
+    }
+}
